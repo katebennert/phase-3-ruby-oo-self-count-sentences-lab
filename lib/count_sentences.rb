9e1d4ec8a9a66ba270692195329fbd7e5a40f4a8
Kate Bennert <kate.bennert@gmail.com>
2023-01-16 12:14:49 -0500

    commit tree e9926fee9d2b9c31ba94e4938c65278f1a664d52

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  # note: this RegEx isn't perfect. Would have to exclude sentence markers that occur inside quotes for example.
   def count_sentences
-
+    self.split(/[.!?]/).filter{|sentence| sentence != ""}.count
   end
 end
